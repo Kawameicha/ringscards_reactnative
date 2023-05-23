8759fa6c0a4231ae89e34e00648fa2092eab6158
Christoph Freier <christoph@freier.fr>
2023-05-23 16:28:38 +0200

    Add icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import {store} from './redux/store';
 import {Tab} from './navigation/RootNavigator';
@@ -9,13 +10,40 @@ import {NavigationContainer} from '@react-navigation/native';
 import {HomeStackScreen} from './navigation/RootNavigator';
 import {SettingsStackScreen} from './navigation/RootNavigator';
 
+const screenOptions = {
+  headerShown: false,
+  tabBarActiveTintColor: '#efb810',
+  tabBarInactiveTintColor: 'black',
+  tabBarStyle: [
+    {
+      display: "flex"
+    },
+    null
+  ]
+}
+
 const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="CardSearch" component={HomeStackScreen} />
-        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen
+          name="Cards"
+          component={HomeStackScreen} 
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <MaterialIcons name="style" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={SettingsStackScreen}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <MaterialIcons name="settings" color={color} size={size} />
+            ),
+          }} />
       </Tab.Navigator>
     </NavigationContainer>
     </Provider>
